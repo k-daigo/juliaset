ee5db007d9807bc0e64356a2d778a7bce0f0a902
Daigokko <k-daigo@kt.rim.or.jp>
2017-03-13 00:04:10 +0900

    整理（不要reg削除など）

--- JuliaCalc.v
+++ JuliaCalc.v
@@ -1,5 +1,5 @@
 `default_nettype none
-//`timescale 1ns/1ms
+
 `include "def.v"
 
 module JuliaCalc(
@@ -14,25 +14,21 @@ module JuliaCalc(
 	output signed [31:0] out_wy,
 	output signed [31:0] out_res);
 
-	reg signed [31:0] a2;
 	reg calc_start = 1'b0;
-	reg calc_end = 1'b0;
 	wire signed [31:0] wx, wy;
 	wire end_flg;
 
 	always @(posedge clk) begin
 		if(enable == 1'b0) begin
 			calc_start <= 1'b0;
-			calc_end <= 1'b0;
 			end_flg <= 1'b0;
 			
-		end else if(enable == 1'b1 & calc_end == 1'b0 && calc_start == 1'b0) begin
+		end else if(enable == 1'b1 && calc_start == 1'b0) begin
 			calc_start <= 1'b1;
 
 			wx <= (((in_x**2) - (in_y**2)) / `JL_MUL) + cr;		// x^2 - y^2 + cr
 			wy <= (((32'sd2 * in_x) * in_y) / `JL_MUL) + ci;	// 2 * x * y + ci
 			
-			calc_end <= 1'b1;
 			end_flg <= 1'b1;
 		end
 	end
